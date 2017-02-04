891b965994eebaa80a2c76ab8e1ab1882fb07bed
Fernando Vilas Maciel <fvilas@gradiant.org>
2017-02-04 09:32:36 +0100

    Removed unnecessary files

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-(sleep 20 ; \
-rabbitmq-plugins enable rabbitmq_web_stomp \
-rabbitmq-plugins enable rabbitmq_event_exchange \
-rabbitmq-plugins enable rabbitmq_management ;) &
-
-rabbitmq-server $@
